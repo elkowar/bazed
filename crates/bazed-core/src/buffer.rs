60e766d30126c654d75fd473ec202f24ff051128
elkowar <5300871+elkowar@users.noreply.github.com>
2022-12-18 14:01:39 +0100

    test(core): Add tests for basic insert, delete and move operations

--- crates/bazed-core/src/buffer.rs
+++ crates/bazed-core/src/buffer.rs
@@ -247,3 +247,35 @@ impl CursorPosition {
         Self { col, ..*self }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_insert() {
+        let mut b = Buffer::open_ephemeral();
+        b.insert_at_carets("hel");
+        b.insert_at_carets("lo");
+        assert_eq!("hello", b.content_to_string());
+    }
+
+    #[test]
+    fn test_backspace() {
+        let mut b = Buffer::open_ephemeral();
+        b.insert_at_carets("a");
+        assert_eq!("a", b.content_to_string());
+        b.delete_backward_at_carets();
+        assert_eq!("", b.content_to_string());
+        b.delete_backward_at_carets();
+        assert_eq!("", b.content_to_string());
+    }
+
+    #[test]
+    fn test_move_caret_into_shorter_line() {
+        let mut b = Buffer::open_ephemeral();
+        b.insert_at_carets("hi\nworld");
+        b.apply_movement_op(MovementOp::Up);
+        b.insert_at_carets(",");
+        assert_eq!("hi,\nworld", b.content_to_string());
+    }
+}
